eece67b0b34ed38000b461f339d22d68bb99afb8
Shashwat Agarwal <shashwat015@users.github.com>
2020-09-17 15:42:23 +0530

    commit

--- app/src/main/java/agarwal/shashwat/mvvm/MainActivity.java
+++ app/src/main/java/agarwal/shashwat/mvvm/MainActivity.java
@@ -7,12 +7,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.util.List;
 
   public class MainActivity extends AppCompatActivity {
 
+      private static final String TAG = "MainActivity";
     private NoteViewModel noteViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +27,11 @@ import java.util.List;
 
         final NoteAdapter noteAdapter=new NoteAdapter();
         recyclerView.setAdapter(noteAdapter);
-
-        noteViewModel= new ViewModelProvider(this).get(NoteViewModel.class);
+         try{
+        noteViewModel= new ViewModelProvider(this).get(NoteViewModel.class);}
+         catch (Exception e){
+             Log.d(TAG, "onCreate: "+e.toString());
+         }
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
